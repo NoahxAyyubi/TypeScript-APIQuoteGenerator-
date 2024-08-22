8ef759129c043eeda2e036562265e625689069bd
Noah Muhammad <noahxmuhammad@gmail.com>
2024-08-22 02:20:25 -0400

    need updates

---
+++ src/App.tsx
@@ -0,0 +1,81 @@
+import { useState } from 'react';
+import {  FaQuoteLeft, FaQuoteRight } from 'react-icons/fa';
+import './App.css';
+
+interface Quote {
+  _id: string;
+  content: string;
+  author: string;
+  authorSlug: string;
+  length: number;
+  tags: string[];
+}
+
+// Function to fetch a random quote
+const fetchRandomQuote = async (): Promise<Quote> => {
+  const response = await fetch('https://api.quotable.io/quotes/random');
+  const [data] = await response.json();
+  return data;
+};
+
+// Function to fetch a quote by tag
+const fetchQuoteByTag = async (tag: string): Promise<Quote> => {
+  const response = await fetch(`https://api.quotable.io/quotes?tags=${tag}`);
+  const data = await response.json();
+  const quotes = data.results;
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  return quotes[randomIndex];
+};
+
+const getColor = (): string => {
+  const red = Math.floor(Math.random() * 128);
+  const green = Math.floor(Math.random() * 128);
+  const blue = Math.floor(Math.random() * 128);
+  return `rgb(${red},${green},${blue})`;
+}
+
+function App() {
+  const [quote, setQuote] = useState<Quote | null>(null);
+  const [color, setColor] = useState<string>(getColor());
+
+  // Function to generate a random quote
+  const generateRandomQuote = async () => {
+    const newQuote = await fetchRandomQuote();
+    setQuote(newQuote);
+    setColor(getColor());
+  }
+
+  // Function to handle emotion button clicks
+  const handleEmotionClick = async (tag: string) => {
+    const newQuote = await fetchQuoteByTag(tag);
+    setQuote(newQuote);
+    setColor(getColor());
+  }
+
+  return (
+    <div className='background' style={{ backgroundColor: color }}>
+      <div id="quote-box">
+        <div className="quote-content" style={{ color: color }}>
+          <FaQuoteLeft size="30" style={{ marginRight: "10px" }} />
+          <h2 id="text" style={{ margin: "5px" }}>
+            {quote ? quote.content : 'Loading...'}
+          </h2>
+          <FaQuoteRight size="30" style={{ marginLeft: "auto" }} />
+          <h4 id="author">{quote ? quote.author : ''}</h4>
+        </div>
+        <div className='generate-random-button'>
+          <button id="new-quote" onClick={generateRandomQuote}>
+            Generate Random Quote
+          </button>
+        </div>
+        <div className="emotion-buttons">
+          <button onClick={() => handleEmotionClick('motivational')}>Sigma Motivation</button>
+          <button onClick={() => handleEmotionClick('wisdom')}>A fool I am make me wise.</button>
+          <button onClick={() => handleEmotionClick('faith')}>Give me Faith</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
